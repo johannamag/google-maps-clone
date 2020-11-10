0530381e74ab7ce548f2e2a0a1cea019c9fca53d
johannamag <69107842+johannamag@users.noreply.github.com>
2020-11-10 20:33:41 +0100

    Added Get Current Location

--- index.js
+++ index.js
@@ -1,6 +1,23 @@
 mapboxgl.accessToken =
   "pk.eyJ1Ijoiam9oYW5uYW1hZyIsImEiOiJja2hjY3N5bXowZ2tzMnNvZ3NiMm9hZHAyIn0.CHbGnXDMeh4MG2-QDagFiQ";
-var map = new mapboxgl.Map({
-  container: "map",
-  style: "mapbox://styles/mapbox/streets-v11",
+
+navigator.geolocation.getCurrentPosition(successLocation, errorLocation, {
+  enableHighAccuracy: true,
 });
+
+function successLocation(position) {
+  setupMap([position.coords.longitude, position.coords.latitude]);
+}
+
+function errorLocation() {
+  setupMap([-2.24, 53.48]);
+}
+
+function setupMap(center) {
+  var map = new mapboxgl.Map({
+    container: "map",
+    style: "mapbox://styles/mapbox/streets-v11",
+    center: center,
+    zoom: 15,
+  });
+}
