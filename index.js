365a983fe1c6bfacf22d16f30e0a480f6c788822
johannamag <69107842+johannamag@users.noreply.github.com>
2020-11-10 20:22:30 +0100

    Project SetUp with Mapbox

---
+++ index.js
@@ -0,0 +1,6 @@
+mapboxgl.accessToken =
+  "pk.eyJ1Ijoiam9oYW5uYW1hZyIsImEiOiJja2hjY3N5bXowZ2tzMnNvZ3NiMm9hZHAyIn0.CHbGnXDMeh4MG2-QDagFiQ";
+var map = new mapboxgl.Map({
+  container: "map",
+  style: "mapbox://styles/mapbox/streets-v11",
+});
